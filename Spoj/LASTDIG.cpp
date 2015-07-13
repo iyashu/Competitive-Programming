1b53f91cf6d2013fb44fc873bcf3435069161c84
Yashpal <yashpal.c1995@gmail.com>
2015-07-13 19:56:59 +0530

    Create LASTDIG.cpp
    Simple Maths, Adhoc

---
+++ Spoj/LASTDIG.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    while(n--)
+    {
+        int a,b;
+        cin>>a>>b;
+        a=a%10;
+        if(b==0) cout<<"1"<<endl;
+        else if(a==(0||1||5||6)&&b!=0) cout<<a<<endl;
+        else if (b%4==0&&b!=0) cout<<(a*a*a*a)%10<<endl;
+        else if(b%4==1)cout<<a%10<<endl;
+        else if(b%4==2)cout<<(a*a)%10<<endl;
+        else if (b%4==3)cout<<(a*a*a)%10<<endl;
+    }
+    return 0;
+}
