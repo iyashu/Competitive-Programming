0f1bf4ec7bc82a1bc685c004c7eec6e23e8ebc9b
Yashpal <yashpal.c1995@gmail.com>
2015-07-13 15:18:21 +0530

    Create DCEPC12H.cpp
    Simple Maths, Adhoc

---
+++ Spoj/DCEPC12H.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+int t;
+cin>>t;
+while(t--)
+{
+    double n,k;
+    scanf("%lf%lf",&n,&k);
+    n = n+1.00;
+    n = n/2.0;
+    printf("%0.6lf\n",n);
+}
+}
