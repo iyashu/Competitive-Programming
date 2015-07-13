bb1a0bc9858df67cb1d295ca5042566cf3d3177d
Yashpal <yashpal.c1995@gmail.com>
2015-07-13 13:24:55 +0530

    Create ALICESIE.cpp
    Simple Maths, Adhoc

---
+++ Spoj/ALICESIE.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x;
+    cin>>x;
+    while(x--)
+    {
+        int n;
+        cin>>n;
+        cout<<n-(n/2)<<endl;
+    }
+    return 0;
+}
