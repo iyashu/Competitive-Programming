e8d740bb7e057aabd353baeeed3d410fc057f344
Yashpal <yashpal.c1995@gmail.com>
2015-07-13 17:48:43 +0530

    Create IITKWPCF.cpp
    Simple Maths, Adhoc

---
+++ Spoj/IITKWPCF.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+long long gcd(long long x, long long y)
+{
+	while(y) y^=x^=y^=x%=y;
+	return x;
+}
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+        long long n,p,i;
+        scanf("%lld",&n);
+        p=n/2;
+        for( i=p;i>=1;i--)
+        {
+            if(gcd(i,n)==1){printf("%lld\n",i);break;}
+        }
+    }
+}
