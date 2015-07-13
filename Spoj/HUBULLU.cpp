851e69686e4c394fee4226147331cf12a353c9bb
Yashpal <yashpal.c1995@gmail.com>
2015-07-13 17:43:58 +0530

    Create HUBULLU.cpp
    Simple Maths, Adhoc

---
+++ Spoj/HUBULLU.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+using namespace std;
+int main()
+{
+	int n, f, t;
+	char player[2][20] = {"Airborne wins.", "Pagfloyd wins."};
+	scanf("%d", &t);
+	while(t--)
+	{
+		scanf("%d %d", &n, &f);
+		puts(player[f]);
+	}
+	return 0;
+}
